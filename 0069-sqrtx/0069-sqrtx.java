e7069c1be2818c4832206da0b189b4d4660a055b
ABISHEK007-R <abishek011206abi@gmail.com>
2026-07-24 10:13:49 +0530

    Time: 1 ms (97.44%), Space: 42.7 MB (42.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==1) return 1;
         long low=1;
-        long high=x/2;
-        long ans=0;
-        while(low<=high){
-        long mid=low+(high-low)/2;
-         long temp=mid*mid;
-        if(temp==x)
+        long high=x;
+        long mid;
+        while(low<=high)
         {
+            mid=low+(high-low)/2;
+            long temp=mid*mid;
+            if(temp==x)
             return (int)mid;
-        }
-        if(temp<x){
-            low=mid+1;
-            ans=mid;
-        }
-        else{
+            else if(temp>x)
             high=mid-1;
+            else
+            low=mid+1;
         }
-        }
-        return (int)ans;
+        return (int)high;
     }
 }
