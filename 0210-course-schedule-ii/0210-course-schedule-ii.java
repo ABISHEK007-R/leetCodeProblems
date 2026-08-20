82a87d33458357824616771b9233f5cdf555746f
ABISHEK007-R <abishek011206abi@gmail.com>
2026-08-20 11:02:33 +0530

    Time: 9 ms (9.81%), Space: 46.8 MB (52.83%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    int arr[]=new int[numCourses];
+    List<List<Integer>> map=new ArrayList<>(numCourses);
+    for(int i=0;i<numCourses;i++){
+        map.add(new ArrayList<>());
+    }
+    List<Integer> ans=new ArrayList<>();
+    for(int[] a:prerequisites){
+        int u=a[1];
+        int v=a[0];
+        arr[v]++;
+        map.get(u).add(v);
+    }
+    Queue<Integer> topo=new LinkedList<>();
+    for(int i=0;i<numCourses;i++){
+       if(arr[i]==0)
+       {
+        topo.offer(i);
+       }
+    }
+    while(!topo.isEmpty()){
+        int k=topo.poll();
+        ans.add(k);
+        for(int i=0;i<map.get(k).size();i++){
+            if(--arr[map.get(k).get(i)]==0){
+             topo.offer(map.get(k).get(i));
+            }
+        }
+    }
+    if(ans.size()!=numCourses){
+        return new int[0];
+    }
+    else{
+        int[] myArray = ans.stream().mapToInt(Integer::intValue).toArray();
+        return myArray;    
+    }
+}
+}
