5e1f6983868bd7cf68fea7ade4442dc4bca69c06
ABISHEK007-R <abishek011206abi@gmail.com>
2026-08-22 10:20:08 +0530

    Time: 0 ms (100%), Space: 46 MB (78.35%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+    double converted[]=new double[2];
+    converted[0]=celsius + 273.15;
+    converted[1]=celsius * 1.80 + 32.00;
+    return converted;     
+    }
+}
